56066dea91b89417472d8a9b9794c9528aa680f6
lucastarche <43870496+lucastarche@users.noreply.github.com>
2019-05-09 22:05:03 -0300

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,17 @@
+var currency = 0;
+var clickingPower = 1;
+var upgradeCost = 10;
+function addClick(){
+    currency += clickingPower;
+    document.getElementById("clicks").textContent = "Clicks: " + currency;
+}
+function increaseClicking(){
+    if (currency >= upgradeCost){
+        clickingPower+=1;
+        currency -= upgradeCost;
+        upgradeCost = parseInt(upgradeCost*1.1)+1;
+    }
+    document.getElementById("clicks").textContent = "Clicks: " + currency;
+    document.getElementById("clickPower").textContent = "Clicking Power: " + clickingPower;
+    document.getElementById("upgradePrice").textContent = "Cost: " + upgradeCost;
+}
