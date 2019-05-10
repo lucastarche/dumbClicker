4af1425282f0620c048941bb18188e2a60154b5f
lucastarche <43870496+lucastarche@users.noreply.github.com>
2019-05-10 11:08:59 -0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,40 +1,40 @@
 var mainGame = {
-     currency = 0;
-     clickingPower = 1;
-     upgradeCost = 10;
-     autoClickers = 0;
-     autoClickersCost = 50;
+     currency: 0,
+     clickingPower: 1,
+     upgradeCost: 10,
+     autoClickers: 0,
+     autoClickersCost: 50,
 }
 function addClick(){
     mainGame.currency += mainGame.clickingPower;
     if (mainGame.currency > 1000){
     document.getElementById("reward").textContent = "This is NOT a Jojo´s reference";
     }
-    uodate();
+    update();
 }
 function increaseClicking(){
-    if (currency >= upgradeCost){
-        clickingPower+=1;
-        currency -= upgradeCost;
-        upgradeCost = parseInt(upgradeCost*1.1)+1;
+    if (mainGame.currency >= mainGame.upgradeCost){
+        mainGame.clickingPower+=1;
+        mainGame.currency -= mainGame.upgradeCost;
+        mainGame.upgradeCost = parseInt(mainGame.upgradeCost*1.1)+1;
     }
 }
 function increaseAutoClickers(){
-    if (currency >= autoClickersCost){
-        autoClickers++;
-        currency -= autoClickersCost;
-        autoClickersCost = parseInt(autoClickersCost*1.1)+1;
+    if (mainGame.currency >= mainGame.autoClickersCost){
+        mainGame.autoClickers++;
+        mainGame.currency -= mainGame.autoClickersCost;
+        mainGame.autoClickersCost = parseInt(mainGame.autoClickersCost*1.1)+1;
     }
 }
 function autoClicker(){
-    currency += autoClickers;
+    mainGame.currency += mainGame.autoClickers;
 }
 function update(){
-    document.getElementById("clicks").textContent = "Clicks: " + currency;
-    document.getElementById("clickPower").textContent = "Clicking Power: " + clickingPower;
-    document.getElementById("upgradePrice").textContent = "Cost: " + upgradeCost;
-    document.getElementById("autoClickers").textContent = "Autoclickers: " + autoClickers;
-    document.getElementById("autoClickersCost").textContent = "Cost: " + autoClickersCost;
+    document.getElementById("clicks").textContent = "Clicks: " + mainGame.currency;
+    document.getElementById("clickPower").textContent = "Clicking Power: " + mainGame.clickingPower;
+    document.getElementById("upgradePrice").textContent = "Cost: " + mainGame.upgradeCost;
+    document.getElementById("autoClickers").textContent = "Autoclickers: " + mainGame.autoClickers;
+    document.getElementById("autoClickersCost").textContent = "Cost: " + mainGame.autoClickersCost;
 }
 window.setInterval(function(){
     autoClicker();
