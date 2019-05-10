fd069ef5cbf9f6aa4c6f021f555c70ce03ca0465
lucastarche <43870496+lucastarche@users.noreply.github.com>
2019-05-10 19:20:04 -0300

    Update main.js

--- main.js
+++ main.js
@@ -41,6 +41,7 @@ function upgradeAutoClickers(){
      }
 }
 function update(){
+    clicksPerSecond = mainGame.autoClickers*(Math.pow(2, mainGame.autoClickersMuliplier));
     document.getElementById("clicks").innerHTML = "Clicks: " + mainGame.currency;
     document.getElementById("clickPower").innerHTML = "Clicking Power: " + mainGame.clickingPower;
     document.getElementById("upgradeClickCost").innerHTML = "Cost: " + mainGame.upgradeClickCost;
@@ -50,7 +51,6 @@ function update(){
     document.getElementById("upgradeAutoClickersCost").innerHTML = "Cost: " + mainGame.upgradeAutoClickersCost;
 }
 window.setInterval(function(){
-    clicksPerSecond = mainGame.autoClickers*(Math.pow(2, mainGame.autoClickersMuliplier));
     autoClicker();
     update();
 }, 1000);
